de0170663083065af4a6e01767d41f4b1bce83be
ke1i901 <pcy.emily@gmail.com>
2026-03-10 00:01:50 +0800

    Color.fromARGB(255, 133, 109, 160);

--- 115502/jpn_learning_app/lib/screens/home/home_screen.dart
+++ 115502/jpn_learning_app/lib/screens/home/home_screen.dart
@@ -22,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   // 設計圖裡的顏色
   final Color _goalGreen = const Color(0xFF6AA86B);
-  final Color _cardRed = const Color(0xFFE58888);
+  final Color _cardRed = const Color.fromARGB(255, 133, 109, 160);
   final Color _cardBlue = const Color(0xFF85B8D6);
   final Color _textColor = const Color(0xFF333333);
   final Color _subTextColor = const Color(0xFF888888);

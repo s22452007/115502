0472fd70512d2c5077400b6773efa8a37304df17
ke1i901 <pcy.emily@gmail.com>
2026-03-16 11:54:48 +0800

    好友列表跳轉

--- 115502/jpn_learning_app/lib/screens/friends/myfriends_screen.dart
+++ 115502/jpn_learning_app/lib/screens/friends/myfriends_screen.dart
@@ -56,13 +56,13 @@ class _FriendsListScreenState extends State<FriendsListScreen> {
           style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
         ),
         actions: [
-          // 🌟 右上角的捷徑：直接跳去加好友！
+          // 🌟 右上角的捷徑：直接跳去好友列表！
           IconButton(
-            icon: Icon(Icons.person_add_outlined, color: _darkGreen, size: 28),
+            icon: Icon(Icons.people_outline, color: _darkGreen, size: 28),
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (_) => const AddFriendScreen()),
+                MaterialPageRoute(builder: (_) => const FriendsListScreen()),
               );
             },
           ),

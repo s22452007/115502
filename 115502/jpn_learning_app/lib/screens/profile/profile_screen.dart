d14b270cf3c7a351fddd4608b2bf5e0330f6cbc3
ke1i901 <pcy.emily@gmail.com>
2026-03-18 23:04:16 +0800

    個人檔案-2

--- 115502/jpn_learning_app/lib/screens/profile/profile_screen.dart
+++ 115502/jpn_learning_app/lib/screens/profile/profile_screen.dart
@@ -224,7 +224,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
         ),
         title: IconButton(
-          icon: Icon(Icons.camera_alt_outlined, color: _primaryGreen, size: 28),
+          icon: const Icon(
+            Icons.camera_alt_outlined,
+            color: Colors.white,
+            size: 28,
+          ),
           onPressed: () {
             Navigator.push(
               context,

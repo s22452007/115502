cfd73803575da217c7e2b3a41e600e549f2754d5
11156048 <11156047@ntub.edu.tw>
2026-03-29 22:07:27 +0800

    增加字體大小

---
+++ 115502/jpn_learning_app/lib/providers/font_size_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FontSizeProvider extends ChangeNotifier {
+  double _scale = 1.0;
+  double get scale => _scale;
+
+  static const _key = 'font_size_scale';
+
+  Future<void> load() async {
+    final prefs = await SharedPreferences.getInstance();
+    _scale = prefs.getDouble(_key) ?? 1.0;
+    notifyListeners();
+  }
+
+  Future<void> setScale(double value) async {
+    _scale = value;
+    notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble(_key, value);
+  }
+}

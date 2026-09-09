7e8c6e9ca368409026a7520835f674783da94d4f
ke1i901 <pcy.emily@gmail.com>
2026-09-09 22:27:06 +0800

    main導入前導

--- 115502/jpn_learning_app/lib/main.dart
+++ 115502/jpn_learning_app/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:jpn_learning_app/screens/auth/welcome_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/gestures.dart'; // 加上這行
 
-import 'package:jpn_learning_app/screens/auth/splash_screen.dart';
+
+
 import 'package:jpn_learning_app/utils/constants.dart';
 import 'package:jpn_learning_app/providers/user_provider.dart';
 import 'package:jpn_learning_app/providers/font_size_provider.dart';
@@ -75,7 +77,7 @@ class JpnLearningApp extends StatelessWidget {
           child: child!,
         );
       },
-      home: const SplashScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
